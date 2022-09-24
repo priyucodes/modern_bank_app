2ef0750212fdbf136b08040add2c2fd8bb76e46f
Priyanshu Bhandari <technopriyu@gmail.com>
2022-09-24 12:57:24 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-export default App
+import styles from './style';
+import {
+  Navbar,
+  Billing,
+  Business,
+  CTA,
+  CardDeal,
+  Clients,
+  Footer,
+  Hero,
+  Stats,
+  Testimonials,
+} from './components';
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    {/* All Components */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
